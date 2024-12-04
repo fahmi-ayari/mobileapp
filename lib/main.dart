b77b2d994b5fab7a6dfbb7bda5dfd2633e7af766
fahmi-ayari <fahmiayari1276@gmail.com>
2024-12-04 17:11:48 +0100

    fix changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:campingbazar/widgets/campingbazar.dart';
 
 void main() {
-  runApp(const CampingBazzarApp());
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: CampingBazzarApp(), // Replace with your app's entry point
+  ));
 }
